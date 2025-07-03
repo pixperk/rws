a2b0790bc6da4609f58127dc25320312a568bf9c
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-07-03 20:07:29 +0530

    refactor : abstract server logic into lib

---
+++ rws-server/src/lib.rs
@@ -0,0 +1,71 @@
+use std::{collections::HashMap,sync::Arc};
+use tokio::{net::{TcpListener, ToSocketAddrs}, sync::Mutex};
+use futures_util::StreamExt;
+use tokio_tungstenite::accept_async;
+use rws_common::EventMessage;
+
+use crate::{client::{Client, Clients}, dispatcher::dispatch, room::RoomManager};
+
+mod dispatcher;
+mod handler;
+mod client;
+mod util;
+mod room;
+
+pub struct Server {
+    addr: String,
+    clients: Clients,
+    room_manager: Arc<Mutex<RoomManager>>,
+}
+
+impl Server {
+    pub async fn bind(addr: impl ToSocketAddrs + ToString) -> anyhow::Result<Self> {
+        // test binding for early errors
+        TcpListener::bind(&addr).await?;
+
+        Ok(Self {
+            addr: addr.to_string(),
+            clients: Arc::new(Mutex::new(HashMap::new())),
+            room_manager: Arc::new(Mutex::new(RoomManager::new())),
+        })
+    }
+
+    pub async fn run(self) -> anyhow::Result<()> {
+        let listener = TcpListener::bind(&self.addr).await?;
+        println!("Starting RWS server on ws://{}...", self.addr);
+
+        while let Ok((stream, _)) = listener.accept().await {
+            let clients = Arc::clone(&self.clients);
+            let room_manager = Arc::clone(&self.room_manager);
+
+            tokio::spawn(async move {
+                let ws_stream = accept_async(stream).await.unwrap(); // WebSocket upgrade
+                let id = uuid::Uuid::new_v4();
+
+                println!("New client connected: {}", id);
+                let (write, mut read) = ws_stream.split();
+                let tx = Arc::new(Mutex::new(write));
+                let client = Client {
+                    id,
+                    username: None,
+                    tx: tx.clone(),
+                };
+
+                clients.lock().await.insert(id, client.clone());
+
+                while let Some(Ok(msg)) = read.next().await {
+                    if msg.is_text() {
+                        if let Ok(msg_obj) = serde_json::from_str::<EventMessage>(&msg.to_string()) {
+                            dispatch(msg_obj, id, &clients, &room_manager).await;
+                        }
+                    }
+                }
+
+                clients.lock().await.remove(&id);
+                println!("Client {} disconnected", id);
+            });
+        }
+
+        Ok(())
+    }
+}
--- rws-server/src/main.rs
+++ rws-server/src/main.rs
@@ -1,63 +1,7 @@
-use std::{collections::HashMap, sync::Arc};
-
-use rws_common::EventMessage;
-use tokio::{net::TcpListener, sync::Mutex};
-use tokio_tungstenite::accept_async;
-use futures_util::stream::{StreamExt};
-
-use crate::{client::{Client, Clients}, dispatcher::dispatch, room::RoomManager};
-
-mod dispatcher;
-mod handler;
-mod client;
-mod util;
-mod room;
-
+use rws_server::Server;
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
-    let room_manager = Arc::new(Mutex::new(RoomManager::new()));
-    
-    println!("Starting RWS server on ws://localhost:3000...");
-
-    while let Ok((stream, _)) = listener.accept().await{
-        let clients = Arc::clone(&clients);
-        let room_manager = Arc::clone(&room_manager);
-        tokio::spawn(async move{
-            let ws_stream = accept_async(stream).await.unwrap(); //upgrade to WebSocket stream
-            let id = uuid::Uuid::new_v4();
-
-
-            println!("New client connected: {}", id);
-            let (write, mut read) = ws_stream.split();
-            let tx = Arc::new(Mutex::new(write));
-            let client = Client{
-                id,
-                username: None,
-                tx: tx.clone(),
-            };
-
-            clients.lock().await.insert(id, client.clone());
-
-            
-
-            // Handle incoming messages
-            while let Some(Ok(msg)) = read.next().await {
-                if msg.is_text() {
-                    if let Ok(msg_obj) = serde_json::from_str::<EventMessage>(&msg.to_string()) {
-                       dispatch(msg_obj, id,  &clients, &room_manager).await;
-                    }
-                }
-            }
-
-                  
-             clients.lock().await.remove(&id);
-            println!("Client {} disconnected", id);
-           
-             
-        });
-    }
+async fn main() -> anyhow::Result<()> {
+    let server = Server::bind("127.0.0.1:3000").await?;
+    server.run().await
 }
-
